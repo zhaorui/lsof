f1db605d44ba6f7c7380deb1269bcdd5e651f37e
赵睿75231 <75231@sangfor.com>
2021-12-27 11:19:57 +0800

    Fix build failed

--- lsof/build-osx.sh
+++ lsof/build-osx.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 export LSOF_INCLUDE=$(xcrun -show-sdk-path)/usr/include
-lsof/Configure -n darwin
+./Configure -n darwin
+sed -i '' 's/dnexus.c//g' Makefile
+sed -i '' 's/dnexus.o//g' Makefile
+sed -i '' 's/dnexus.c//g' dialects/darwin/libproc/Makefile
+sed -i '' 's/dnexus.o//g' dialects/darwin/libproc/Makefile
 make
